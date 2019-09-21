6ca167f15dc9a59f2c39abe3c322d8fee193cd1a
Becavalier <yhorg@hotmail.com>
2019-09-21 22:51:36 +0800

    misc: rebuilt.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,7 +3,7 @@
     <div class="form">
       <p class="header">
         <span>企业用户登录</span>
-        <el-button type="text">个人用户</el-button>
+        <el-button type="text" @click="gotoCustomerSite">个人用户</el-button>
       </p>
       <el-form ref="form" :rules="rules" :model="form" label-width="80px">
         <el-form-item label="手机号" prop="username">
@@ -87,6 +87,10 @@ export default class Home extends Vue {
   gotoResetPwdUI() {
     this.$router.push({ path: '/reset-password' });
   };
+
+  gotoCustomerSite() {
+    window.open('http://47.102.145.186/client/#/home');
+  }
   
 }
 </script>
